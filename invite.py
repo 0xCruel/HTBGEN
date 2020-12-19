0ea63b6632a9beed03dd04d7cbbb5eefe17de589
cruel <75235291+CruelSec@users.noreply.github.com>
2020-12-19 12:48:28 -0500

    Create invite.py

---
+++ invite.py
@@ -0,0 +1,37 @@
+import base64
+import subprocess
+from colorama import Fore, Back, Style
+
+
+def banner():
+
+    g = Fore.GREEN
+    w = Fore.WHITE
+
+    print(f"""
+   
+   
+                                        {g}     __  ________ ____  _____________   __
+                                        {w}    / / / /_  __/ __ )/ ____/ ____/ | / /
+                                        {g}   / /_/ / / / / __  / / __/ __/ /  |/ / 
+                                        {w}  / __  / / / / /_/ / /_/ / /___/ /|  /  
+                                        {g} /_/ /_/ /_/ /_____/\____/_____/_/ |_/   
+                                      
+   
+   
+   
+   
+   """)
+
+banner()
+
+
+subprocess.call("curl -XPOST https://www.hackthebox.eu/api/invite/generate", shell=True) 
+print('\n')
+api = str(input(Fore.RED + "copy the line and past it here:\n==> ")) 
+print('\n')
+dec = str(base64.standard_b64decode(api))
+print("[#]==> " + dec.replace('b', '')) 
+print('\n')
+print("Enjoy!") 
+print('\n')
